0e71d700b2d2f71b866fd1a8121fff4dc8ed6582
Frank Draw <79576936+FrankDraw@users.noreply.github.com>
2024-03-14 14:58:35 +0800

    Update UEFI.bat

--- UEFI.bat
+++ UEFI.bat
@@ -3,6 +3,7 @@ CHCP 65001 >NUL 2>&1 & ECHO Code: UTF-8
 TITLE Reboot To UEFI.
 SETLOCAL ENABLEDELAYEDEXPANSION & CD /D %~DP0
 MODE CON: COLS=45 LINES=15 & COLOR 0A
+REM  Author: Frank Draw (SZ CHINA)
 ECHO.  
 ECHO    *************************************
 ECHO    ***  Please Run as Administrator. ***
@@ -19,7 +20,7 @@ ECHO    This PC will restart after 5 second.
 ECHO    Next Boot to UEFI Settings Page.
 ECHO.
 REM ******* Main Command. *******
-SHUTDOWN /R /T 0 /FW
+%windir%\system32\shutdown.exe /R /T 0 /FW
 
 REM ******* Command End *********
 EXIT
